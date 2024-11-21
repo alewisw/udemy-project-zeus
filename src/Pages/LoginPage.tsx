ccdd716a49b4d4adb58eac3e486622ee44b2ef39
Andrew Lewis <andrew.lewis@firstmode.com>
2024-11-21 11:13:58 +0800

    feat: login component

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,19 +1,10 @@
 import React from "react";
-import Input from "../Components/Input";
+import Login from "../Components/Login";
 
 const LoginPage = () => {
   return (
     <div className="bg-blue-600 h-[100vh] flex items-center justify-center p-10">
-      <div className="w-full md:w-[450px]">
-        <h1 className="text-white text-center font-bold text-4xl md:text-6xl mb-10">
-          Login
-        </h1>
-        <div className="flex flex-col gap-3 bg-white w-full p-6 min-h-[150px] rounded-xl drop-shadow-xl">
-          <Input></Input>
-          <Input></Input>
-          <Input></Input>
-        </div>
-      </div>
+      <Login></Login>
     </div>
   );
 };
